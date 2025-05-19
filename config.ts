b03fde0f2e7c1c12f85de9a24ea8407feb49fc18
AboubakrDiallo <aladji.diallo.7509@gmail.com>
2025-05-19 15:00:46 +0000

    essaie

--- config.ts
+++ config.ts
@@ -1,3 +1,2 @@
 // Configuration de l'API
-export const API_URL = "http://192.168.163.8:3333/api/v1"; // URL de l'API backend
-
+export const API_URL = "http://127.0.0.1:3333/api/v1"; // URL de l'API backend
